36200a321e04554b3b48fabdcb8db9c6a803e7b0
Santoshalimkar <santosh.sa4d@gmail.com>
2024-09-04 19:03:13 +0530

    Changes added

--- components/Managecategory/Addsubcategory.js
+++ components/Managecategory/Addsubcategory.js
@@ -2,51 +2,7 @@ import React from 'react'
 
 const Addsubcategory = () => {
   return (
-    <div> <Card x-chunk="dashboard-07-chunk-0">
-    <CardHeader>
-      <CardTitle>Add Subcategory</CardTitle>
-      <CardDescription>
-        Lipsum dolor sit amet, consectetur adipiscing elit
-      </CardDescription>
-    </CardHeader>
-    <CardContent>
-      <div className="grid gap-6 w-full ">
-        <div className="grid gap-3 ">
-          <Label htmlFor="name">Select Category</Label>
-          <Select>
-            <SelectTrigger className="w-full">
-              <SelectValue placeholder="Select a fruit" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectLabel>Fruits</SelectLabel>
-                <SelectItem value="apple">Apple</SelectItem>
-                <SelectItem value="banana">Banana</SelectItem>
-                <SelectItem value="blueberry">Blueberry</SelectItem>
-                <SelectItem value="grapes">Grapes</SelectItem>
-                <SelectItem value="pineapple">Pineapple</SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-        </div>
-        <div className="grid gap-3">
-          <Label htmlFor="picture">Category image</Label>
-          <Input id="picture" type="file" />
-        </div>
-        <div className="grid gap-3">
-          <Label htmlFor="description">Description</Label>
-          <Textarea
-            id="description"
-            defaultValue="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nisl nec ultricies ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc."
-            className="min-h-24"
-          />
-        </div>
-        <div className="grid gap-3">
-          <Button>Add Category</Button>
-        </div>
-      </div>
-    </CardContent>
-  </Card></div>
+    <div>Addsubcategory</div>
   )
 }
 
